72bb31089d6dcfd051e6881b1ab02ee4201d022e
Yizhen Shi <yshi@duosecurity.com>
2023-09-09 18:40:14 -0400

    first c program

---
+++ FirstProgram/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int myFavNum = 0;
+    printf("please neter your favovrite number\n");
+    scanf("%d", &myFavNum);
+    printf("You entered number: %d\n", myFavNum);
+    return 0;
+}
